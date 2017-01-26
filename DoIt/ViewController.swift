52dd6d4844d82d4fbcbfe4c175a346022435c673
Oliver Beck <Oliver@olivers-mbp.fritz.box>
2017-01-26 23:43:38 +0100

    Initial Commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Oliver Beck on 26.01.17.
+//  Copyright © 2017 back2data-apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
